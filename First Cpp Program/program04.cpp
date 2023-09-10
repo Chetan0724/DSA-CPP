7131fa55122747707181bebefbd515cf8e7beb2a
Chetan Yadav <ydvchetan01@gmail.com>
2023-09-10 13:29:56 +0530

    DSA Lecture 2

---
+++ First Cpp Program/program04.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int marks;
+    cout << "Enter the marks here: " << endl;
+    cin >> marks;
+    cout << "Printing marks: " << marks << endl;
+
+    char ch;
+    cin >> ch;
+    cout << "here: " << ch << endl;
+    return 0;
+}
