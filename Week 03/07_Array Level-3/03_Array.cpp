e1bc8ce2ad0f16ddf51f0cff8a48a53cc5bbb247
Chetan Yadav <ydvchetan01@gmail.com>
2024-02-23 23:56:04 +0530

    Array level 3

---
+++ Week 03/07_Array Level-3/03_Array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[3][3];
+    int row = 3;
+    int col = 3;
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << "Enter the input for row index: " << i << " column index: " << j << endl;
+            cin >> arr[i][j];
+        }
+    }
+
+    return 0;
+}
