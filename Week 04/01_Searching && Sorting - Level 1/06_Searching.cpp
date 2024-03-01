290cffb8e6bc9ea45ee21e42fdd5ce489e7bccd7
Chetan Yadav <ydvchetan01@gmail.com>
2024-03-01 23:49:38 +0530

    missing element problem

---
+++ Week 04/01_Searching && Sorting - Level 1/06_Searching.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int findMissingElement(int arr[], int n) {
+    int s = 0;
+    int e = n - 1;
+    int mid = s + (e - s)/2;
+    int ans = -1;
+
+    while (s <= e)
+    {
+        int diff = arr[mid] - mid;
+
+        if(diff == 1) {
+            s = mid + 1;
+        }
+
+        else
+        {
+            ans = mid;
+            e = mid - 1;
+        }
+        mid = s + (e - s)/2;
+    }
+
+    if (ans + 1 == 0)
+    {
+        return n + 1;
+    }
+    
+    return ans+1;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int n = 8;
+    cout << "Missing element is: " << findMissingElement(arr, n);
+
+    return 0;
+}
