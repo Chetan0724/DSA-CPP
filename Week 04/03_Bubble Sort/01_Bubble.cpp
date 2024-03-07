ed551f1501980527ea363c6eaf3e67f57acc8244
Chetan Yadav <ydvchetan01@gmail.com>
2024-03-07 18:11:18 +0530

    Bubble Sort

---
+++ Week 04/03_Bubble Sort/01_Bubble.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void print(vector<int> &v) {
+    for (int i = 0; i < v.size(); ++i)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+
+void bubbleSort(vector<int> &v) {
+    int n = v.size();
+    for (int i = 0; i < n -1; ++i)
+    {
+        for (int j = 0; j < n - i - 1; ++j)
+        {
+            // if (v[j] > v[j+1]) --> Increasing Order
+            if (v[j] < v[j+1]) //--> Decreasing Order
+            {
+                swap(v[j], v[j+1]);
+            } 
+        }
+    } 
+}
+
+int main()
+{
+    // Bubble Sort
+    // TC --> O(n^2)
+    // SC --> O(1)
+
+    vector<int> v = {5, 3, 0, -2, 11};
+    bubbleSort(v);
+    print(v);
+
+    return 0;
+}
